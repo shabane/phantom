d3cfdd18d3bab9685cd84804ce3e6a634e4356af
shabane <m.mohamadshabane@gmail.com>
2024-12-21 05:48:33 +0330

    feat: change the files name to its original

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,34 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, NextFunction, Request, Response } from "express";
 const app: Express = express();
+
+import multer from "multer";
+// const upload = multer({ dest: "uploads/" });
 
 app.get("/api/file/", (req: Request, res: Response) => {
 	res.send("Post Your Files here");
 });
+
+const storage = multer.diskStorage({
+	destination: function (req: Request, file: any, cb: any) {
+		cb(null, "uploads/");
+	},
+	filename: function (req: Request, file: any, cb: any) {
+		const uniqueSuffix = Date.now() + "-" +
+			Math.round(Math.random() * 1E9);
+		cb(null, uniqueSuffix + "-" + file.originalname);
+	},
+});
+
+const upload = multer({ storage: storage });
+
+app.post(
+	"/api/file/",
+	upload.array("files", 3),
+	(req: Request, res: Response) => {
+		console.log("we are here");
+		res.send("we recived your files");
+	},
+);
 
 app.listen(3000, () => {
 	console.log("App Listen on 3000");
