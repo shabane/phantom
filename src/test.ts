663ffa0b1367c389b58cae040345639f8198a738
shabane <m.mohamadshabane@gmail.com>
2024-12-23 05:33:11 +0330

    test: write test for test the mail server(SMTP)

--- src/test.ts
+++ src/test.ts
@@ -1,3 +1,14 @@
 import { nameIt } from "./config";
+import { mail, sendMail } from "./mailer.ts";
 console.log(nameIt());
+console.log("------------------");
+
+mail.verify((res, rej) => {
+	if (rej) {
+		console.log(rej);
+		return;
+	}
+	console.log(res);
+});
+sendMail("m.mohamadshabane@gmail.com", "test", "text", "<h1> html <h1>");
 console.log("------------------");
