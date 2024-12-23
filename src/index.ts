2c1e880d281963d1b3e5b58b4c6218dcf355983d
shabane <m.mohamadshabane@gmail.com>
2024-12-24 00:23:01 +0330

    feat: sending mail

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import express, { Express, Request, Response } from "express";
 const app: Express = express();
-import { MAX_FILE_UPLOADS, nameIt, UPLOAD_DIR } from "./config";
+import {
+	EMAIL_LINK_THEME,
+	EMAIL_SUBJECT,
+	EMAIL_THEME,
+	MAX_FILE_UPLOADS,
+	nameIt,
+	SITE_DOMAIL,
+	UPLOAD_DIR,
+} from "./config";
 
 import multer from "multer";
 // const upload = multer({ dest: "uploads/" });
+
+import { sendMail } from "./mailer";
 
 // serving uploaded files
 app.use("/file", express.static(UPLOAD_DIR));
@@ -13,11 +23,11 @@ app.get("/api/file/", (_: Request, res: Response) => {
 });
 
 const storage = multer.diskStorage({
-	destination: function(_: Request, file: any, cb: any) {
+	destination: function (_: Request, file: any, cb: any) {
 		cb(null, UPLOAD_DIR);
 	},
 
-	filename: function(_: Request, file: any, cb: any) {
+	filename: function (_: Request, file: any, cb: any) {
 		cb(null, nameIt() + "-" + file.originalname);
 	},
 });
@@ -26,10 +36,18 @@ const upload = multer({ storage: storage });
 
 app.post(
 	"/api/file/",
-	upload.array("files", MAX_FILE_UPLOADS),
+	upload.single("file"),
 	(req: Request, res: Response) => {
-		console.log("we are here");
-		res.send("we recived your files");
+		if (!req.file) {
+			res.status(400).send("please send a file");
+		}
+		sendMail(
+			req.params.to,
+			EMAIL_SUBJECT,
+			EMAIL_THEME + req.file?.path,
+			EMAIL_THEME + req.file?.path,
+		);
+		res.send("your file will send");
 	},
 );
 
